397d99eb2d1eac6ab3546a7c0f2a6b69578dfda3
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-04 19:38:04 +0100

    Variables for Strings

---
+++ 2-variables-for-strings.js
@@ -0,0 +1,115 @@
+//  Chapter 2 - Variables for Strings
+//  =================================
+
+//  1. In the following code, "Mark" is a string. What is name (answer with 1 word)?
+//  Answer:
+//  variable
+
+//  2. In one word, if it is enclosed in quotation marks, what is it?
+//  Answer:
+//  string
+
+//  3. What is the keyword that declares a variable?
+//  Answer:
+//  var (also, let, and const which are prefered over the use of var)
+
+//  4. This statement...
+//  userName = "buddy5000";
+//  ...is correct only if the variable has already been _______.
+//  Answer:
+//  declared
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto 
+//  the screen to form a statement that declares the variable punchline and assigns a value to it.
+//  Answer:
+//  var punchline = "The accountant.";
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  In a single statement, declare the variable nickname and assign the string "Satchmo" to it.
+//  Answer:
+//  var nickname = "Satchmo";
+
+//  7. Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+//  Declare the variable teamName.
+//  Answer:
+//  var teamName;
+
+//  8. In a single statement declare a variable and assign a string of your choice to it. 
+//  (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+//  Answer:
+//  var varOne = "I love coding";
+
+//  9. Declare a variable. (You've learned only one rule for naming variables so far, so it's the 
+//  only one I'll hold you to.)
+//  Answer:
+//  var varOne;
+
+//  10. Assign a string of your choice to a variable of your choice that has already been declared. 
+//  (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+//  Answer:
+//  varThree = "Pizza";
+
+//  11. This statement has already been coded.
+//  var bestMan = "Charlie";
+//  Assign the variable a new string.
+//  Answer:
+//  bestMan = "Not Charlie";
+
+//  12. Declare a variable without defining it. Then, in a second statement, assign it a string. 
+//  (You've learned only one rule for naming variables so far, so it's the only one I'll hold you to.)
+//  Answer:
+//  var testOne;
+//  testOne = "Microphone Check";
+
+//  13. Time goal: 30 seconds. (The timer starts when you click in the answer field.) 
+//  In a single statement declare a variable of your choice and assign a string of your 
+//  choice to it. You've learned only one rule for naming variables so far, so it's the only 
+//  one I'll hold you to. (The timer starts when you click in the answer field.)
+//  Answer:
+//  var varOne = "Test";
+
+//  14. Time goal: 15 seconds. Declare a variable of your choice. (You've learned only one rule for naming 
+//  variables so far, so it's the only one I'll hold you to.)
+//  var varTwo;
+
+//  15. Time goal: 20 seconds. Assign a string of your choice to a variable of your choice 
+//  that has already been declared. (You've learned only one rule for naming variables so far, 
+//  so it's the only one I'll hold you to.)
+//  Answer:
+//  bestMan = "Jeff";
+
+//  16. Time goal: 20 seconds. This statement has already been coded.
+//  var bestMan = "Charlie";
+//  Assign the variable a new string of your choice.
+//  bestMan = "Jeff";
+
+//  17. Time goal: 35 seconds. Declare a variable of your choice without defining it. 
+//  Then, in a second statement, assign it a string of your choice. (You've learned only one rule 
+//  for naming variables so far, so it's the only one I'll hold you to.)
+//  Answer:
+//  var cheeseMaker;
+//  cheeseMaker = "cuts cheese";
+
+//  18. Time goal: 40 seconds. In the first statement declare a variable of your choice and assign 
+//  it a string of your choice. In the second statement change the value of the variable to a 
+//  different string of your choice.
+//  Answer:
+//  var niceFruit = "Apples";
+//  niceFruit = "Pears";
+
+//  19. Live coding exercise: Declare a variable of your choice and assign it a string of your choice. 
+//  Then code an alert, specifying the variable, not the string, as the message. 
+//  Click the Run It button to run your code live. Type out the code, don't copy and paste it. 
+//  If you copy and paste it from the screen showing the correct code, it may not work.
+//  Answer:
+//  var varOne = "coder";
+//  alert(varOne);
+
+//  20. Live coding exercise: Declare a variable of your choice and assign it a string that's 
+//  exactly the same as the variable name, but enclosed in quotation marks so JavaScript recognizes 
+//  it as a string. Then code an alert, specifying the variable, not the string, as the message. 
+//  Click the Run It button to run your code live. Type out the code, don't copy and paste it. 
+//  If you copy and paste it from the screen showing the correct code, it may not work.
+//  Answer:
+//  var varOne = "varOne";
+//  alert(varOne);
