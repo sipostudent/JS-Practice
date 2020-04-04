02f750c025e996094f80ce245f6955b489da7494
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-04 19:40:16 +0100

    Variable Names Legal and Illegal

---
+++ js-exercises/4-variable-names-legal-and-illegal.js
@@ -0,0 +1,117 @@
+//  Chapter 4 - Variable Names Legal and Illegal
+//  ============================================
+
+//  1. Type the illegal character in this statement.
+//  var product cost = 3.45;
+//  Answer:
+//  A space is an illegal character in a variable name.
+
+//  2. Type the illegal character in this statement.
+//  var P2_a%88$;
+//  Answer:
+//  %
+
+//  3. Rewrite this using camelCase.
+//  var Nameofband;
+//  Answer:
+//  var nameOfBand;
+
+//  4. Declare a variable using nothing but all of the legal characters 
+//  that aren't alphabet characters or numbers. Don't repeat any of them.
+//  Answer:
+//  var $_;
+//  var _$;
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces of 
+//  code onto the screen to form a statement that declares a variable in camelCase.
+//  Answer:
+//  var averageHumanIntelligence;
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  In a single statement, declare the variable firstLegalChar and assign it a string. 
+//  The string consists entirely of the first character on the keyboard in the top row that's 
+//  legal in a variable name, as long as the character doesn't come first.
+//  Answer:
+//  var firstLegalChar = "1";
+
+//  7. Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+//  Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+//  Answer:
+//  var booHoo;
+
+//  8. In a single statement declare a legally-named variable and assign a number to it.
+//  Answer:
+//  var favNum = 7;
+
+//  9. In a single statement declare a legally-named variable and assign to it the sum of 2 
+//  other legally-named variables.
+//  Answer:
+//  var varOne = varTwo + varThree;
+
+//  10. Declare a variable whose name is the combination of 3 characters, in this order: a legal 
+//  first character that is not a letter, a character that would be illegal as a first character, 
+//  and a character that would be legal anywhere in the name.
+//  Answer:
+//  var _9w;
+
+//  11. The variable boogieWoogie has already been declared. Assign to it the value of another 
+//  variable, woogieBoogie.
+//  Answer:
+//  boogieWoogie = woogieBoogie;
+
+//  12. Declare a variable that is a combination of your first and last names. Use camelCase.
+//  Answer:
+//  sipoCharles;
+
+//  13. Time goal: 20 seconds. Declare a variable using nothing but all of the legal characters 
+//  that aren't alphabet characters or numbers. Don't repeat any of them. 
+//  (The timer starts when you click in the answer field.)
+//  Answer:
+//  var $_;
+
+//  14. Time goal: 20 seconds. In a single statement declare a legally-named variable and 
+//  assign a number to it.
+//  Answer:
+//  var varOne = 5;
+
+//  15. Time goal: 30 seconds. In a single statement declare a legally-named variable and 
+//  assign to it the sum of 2 other legally-named variables.
+//  Answer:
+//  var varOne = varTwo + varThree;
+
+//  16. Time goal: 25 seconds. Declare a variable whose name is the combination of 3 characters, 
+//  in this order: a legal first character that is not a letter, a character that would be illegal 
+//  as a first character, and a character that would be legal anywhere in the name.
+//  Answer:
+//  var _9w;
+
+//  17. Time goal: 30 seconds. The variable boogieWoogie has already been declared. 
+//  Assign to it the value of another variable, woogieBoogie.
+//  Answer:
+//  boogieWoogie = woogieBoogie;
+
+//  18. Time goal: 20 seconds. Declare a variable that is a combination of your first and last names. 
+//  Use camelCase. If you have a complex name, simplify it. Use "Sarah Billings" instead of 
+// "Sarah Sue Billings-Coopersmith III"
+//  Answer:
+//  var sipoCharles;
+
+//  19. Live coding exercise:
+//  - Declare a variable whose name is your first and last names combined, in camelCase.
+//  - Assign the variable your first and last names, as a string.
+//  - Code an alert, specifying the variable, not the string, as the message.
+//  - Click the Run It button to run your code live.
+//  Answer:
+//  var sipoCharles;
+//  sipoCharles = "Sipo Charles";
+//  alert(sipoCharles);
+
+//  20. Live coding exercise:
+//  - Declare a variable that combines a number, an alphabet letter, and one of the legal characters that are neither an alphabet letter nor a number.
+//  - Assign to the variable a string consisting of another legal character that is neither an alphabet letter nor a number.
+//  - Display the variable's value in an alert, specifying the variable, not the value, as the message.
+//  - Click the Run It button to run your code live.
+//  Answer:
+//  var a_1;
+//  a_1 = "$";
+//  alert(a_1);
