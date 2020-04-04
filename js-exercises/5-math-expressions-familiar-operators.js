fc55e4a6959d313f3e47285372781aaf6e768403
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-04 23:04:35 +0100

    Math expressions: Unfamiliar operators

---
+++ js-exercises/5-math-expressions-familiar-operators.js
@@ -0,0 +1,113 @@
+//  Chapter 5 - Math expressions: Familiar operators
+
+//  1.Enter the operators, in this order, for addition, subtraction, multiplication, 
+//  and division. Don't type spaces between them.
+//  Answer:
+//  +-*/
+
+//  2. What is the name of the arithmetic operator that gives you the remainder when 
+//  one number is divided by another? (one word)
+//  Answer:
+//  modulus
+
+//  3. Type the modulus operator.
+//  Answer:
+//  %
+
+//  4. What is the value of num?
+//  var num = 20 % 6;
+//  Answer:
+//  2
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto 
+//  the screen to form a statement that declares the variable costPerItem and assigns it 
+//  the result of totalCost divided by numberOfItems.
+//  Answer:
+//  var costPerItem = totalCost / numberOfItems;
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  In a single statement, declare the variable largeNum and assign it the result of 1,000 
+//  multiplied by 2,000.
+//  Answer:
+//  var largeNum = 1000 * 2000;
+
+//  7. Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+//  Assign to the variable num the result of dividing 9 by the value represented by the variable 
+//  qty. The variable num hasn't been declared beforehand.
+//  Answer:
+//  var num = 9 / qty;
+
+//  8. Assign to a variable the result of adding a number to itself, for example 2 + 2. 
+//  The variable hasn't been declared beforehand. Make up the variable name.
+//  Answer:
+//  var sum = 1 + 1;
+
+//  9. Assign to a variable the value represented by one variable subtracted from the value 
+//  represented by another variable. The first variable has been declared beforehand. 
+//  Make up all the variable names.
+//  Answer:
+//  var sum = sumOne - sumTwo;
+
+//  10. Code an alert that displays the result of any mathematical operation on 2 variables. 
+//  Make up the variable names.
+//  Answer:
+//  alert(sumOne + sumTwo);
+
+//  11. Assign to a variable the remainder when one number is divided by another. 
+//  The variable hasn't been declared beforehand. Make up the variable name.
+//  Answer:
+//  var remainder = 10 % 3;
+
+//  12. Assign to a variable the remainder when the value represented by a variable 
+//  is divided by a number. The first variable has been declared beforehand. 
+//  Make up the variable names.
+//  Answer:
+//  var remainder = sumOne % 3;
+
+//  13. Time goal: 20 seconds. (The timer starts when you click in the answer field.) 
+//  Enter the operators, in this order, for addition, subtraction, multiplication, 
+//  division, and modulus. Don't type spaces between them. 
+//  (The timer starts when you click in the answer field.)
+//  Answer:
+//  +-*/%
+  
+//  14. Time goal: 25 seconds. Assign to a variable the result of adding a number to itself. 
+//  The variable hasn't been declared beforehand. Make up the variable name.
+//  Answer:
+//  var result = 1 + 1;
+
+//  15. Time goal: 30 seconds. Assign to a variable the value represented by one variable 
+//  subtracted from the value represented by another variable. The first variable has been 
+//  declared beforehand. Make up all the variable names.
+//  Answer:
+//  var result = varOne - varTwo;
+
+//  16. Time goal: 30 seconds. Code an alert that displays the result of any mathematical 
+//  operation on 2 variables. Make up the variable names.
+//  Answer:
+//  alert(sumOne + sumTwo);
+
+//  17. Time goal: 30 seconds. Assign to a variable the remainder when one number is divided 
+//  by another. The variable hasn't been declared beforehand. Make up the variable name.
+//  var remainder = 10 % 3;
+
+//  18. Time goal: 30 seconds. Assign to a variable the remainder when the value represented 
+//  by a variable is divided by a number. The first variable has been declared beforehand. 
+//  Make up the variable names.
+//  Answer:
+//  remainder = varOne % 3;
+
+//  19. Live coding exercise: Code an alert that displays the result of a math operation on 2 numbers.
+//  Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+//  Click the Run It button to run your code live.
+//  Answer:
+//  alert(9 * 9);
+
+//  20. Live coding exercise: Assign numbers to 2 new variables. Code an alert that displays 
+//  the result of a math operation on the variables.
+//  Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+//  Click the Run It button to run your code live.
+//  Answer:
+//  var varOne = 5;
+//  var varTwo = 10;
+//  alert(varOne + varTwo);
