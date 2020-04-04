488633969320ce161eaaba025bd8d77bb51f84d5
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-04 19:34:39 +0100

    Alerts

---
+++ js-exercises/1-alerts.js
@@ -0,0 +1,102 @@
+//  Chapter 1 - Alerts
+//  ==================
+
+//  I'll give you one exercise at a time. After you give your answer for each exercise, 
+//  I'll give you another exercise. When you get one wrong, I'll let you know. 
+//  Later in the session, I'll circle back to that exercise to give you another try.
+
+//  Challenges
+//  ----------
+
+//  1. What is the keyword that displays a message box?
+//  Answer:
+//  alert
+
+//  2. Type the character that ends every statement.
+//  Answer:
+//  ;
+
+//  3. Type the 2 characters that are missing in this statement.
+//  alert"You're learning JavaScript!";
+//  Answer:
+//  ()
+
+//  4. A __________ ends in a semicolon.
+//  Answer:
+//  statement
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the 
+//  screen to form an alert.
+//  Answer:
+//  alert("Hi there, Sailor!");
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  Code an alert that displays the message "Thanks for everything!"
+//  Answer:
+//  alert("Thanks for everything!");
+
+//  7. Fail-safe coding. if you type the wrong character, Ill cancel the keystroke, Type spaces.
+//  Code an alert that displays the message "Boo!"
+//  Answer:
+//  alert("Boo!");
+
+//  8. The code displays a box with a message in it. Fill in the blank.
+//  ______("You won!");
+//  Answer:
+//  alert
+
+//  9. The code displays a box with a message in it. Fill in the blank.
+//  alert("You won!____
+//  Answer:
+//  alert("You won!")
+
+//  10. Code an alert that displays the message "Hi"
+//  Answer:
+//  alert("Hi");
+
+//  11. What are the 5 punctuation characters of an alert statement, in order?
+//  Answer:
+//  ("");
+
+//  12. Code an alert statement displaying any message you like.
+//  Answer:
+//  alert("Coding the Myers way is fun!");
+
+//  13. Time goal: 12 seconds. (The timer starts when you click in the answer field.) 
+//  The code displays a box with a message in it. Fill in the blank.
+//  ______("You won!");
+//  Answer:
+//  alert
+
+//  14. Time goal: 10 seconds. The code displays a box with a message in it. Fill in the blank.
+//  alert("You won!____
+//  Answer:
+//  ");
+
+//  15. Time goal: 20 seconds. Code an alert that displays the message "Hi"
+//  Answer:
+//  alert("Hi");
+
+//  16. Time goal: 20 seconds. What are the 5 punctuation characters of an alert statement, in order?
+//  Answer:
+//  ("");
+
+//  17. Time goal: 25 seconds. Code an alert statement displaying any message you like.
+//  Answer:
+//  alert("Hello");
+
+//  18. Time goal: 25 seconds. Code an alert statement that displays your full name.
+//  Answer:
+//  alert("Sipo Charles");
+
+//  19. Live coding exercise: Code an alert with the message "Hello world!" Then click 
+//  the Run It button to run your code live. Type out the code. If you copy and paste it 
+//  from the screen showing the correct code, it may not work.
+//  Answer:
+//  alert("Hello world!");
+
+//  20. Live coding exercise: Code an alert with any message you like. Then click the 
+//  Run It button to run your code live. Type out the code. If you copy and paste it 
+//  from the screen showing the correct code, it may not work.
+//  Answer:
+//  alert("Hello again world!");
