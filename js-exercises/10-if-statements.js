41070d7686f3c88df55f32a8c81028beb80f8f1f
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-06 00:47:30 +0100

    if Statements

---
+++ js-exercises/10-if-statements.js
@@ -0,0 +1,179 @@
+//  Chapter 10 - if Statements
+//  ==========================
+
+//  1. What are the first 4 characters of an if statement?
+
+//  Answer:
+
+//  if ()
+
+//  2. What character ends the first line of an if statement?
+
+//  Answer:
+
+//  {
+
+//  3. What is the last line of an if statement?
+
+//  Answer:
+
+//  }
+
+//  4. if (city = "Las Vegas") {
+//  In the above statement, what character is incorrect?
+
+//  Answer:
+
+//  =
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces to create 
+//  this if statement: If a has the value of b, x has the value of y.
+
+//  Answer:
+
+//  if (a === b) {
+//    x = y;
+//  }
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  Code the first line of an if statement that tests whether teammates has the value of 8.
+
+//  Answer:
+
+//  if (teammates === 8) {
+
+//  7. Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces and carriage returns.
+//  This is the first line of an if statement.
+
+//  if (firstName === "John") {
+
+//  Complete the if statement. If the condition is true, display an alert that says, "Hi, John".
+
+//  Answer:
+//    alert("Hi, John");
+//  }
+
+//  8. Code the first line of an if statement that tests whether one variable has the 
+//  same value as another.
+
+//  Answer:
+
+//  if (x === x) {
+
+//  9. Code the first line of an if statement that tests whether a variable has the 
+//  value of a particular string.
+
+//  Answer:
+
+//  if (name === "Sipo") {
+
+//  10. This is the first line of an if statement:
+
+//  if (x === y) {
+
+//  Complete the statement. If the condition is true, display a box that asks the user a question. 
+//  Assign the answer to a variable that hasn't been declared beforehand.
+
+//  Answer:
+
+//      var answer = prompt("What is z?");
+//  }
+
+//  11. Code an if statement that tests whether pCode is "10010". If so, assign to the 
+//  variable city the string "New York". city has already been declared.
+
+//  Answer:
+
+//  if (pCode === "10010") {
+//       city = "New York";
+//  }
+
+//  12. Code an if statement. Test whether a variable has a particular numerical value. 
+//  If so, assign a new value to that variable, as in x = 1;
+
+//  Answer:
+
+//  if (num === 1) {
+//      num = 4;
+//  }
+
+//  13. Time goal: 15 seconds. (The timer starts when you click in the answer field.)
+//  What are the 4 bracket characters in every if statement, in order?.
+
+//  Answer:
+
+//  (){}
+
+//  14. Time goal: 35 seconds. Code the first line of an if statement that tests whether 
+//  one variable has the same value as another.
+
+//  Answer:
+
+//  if (firstName === lastName) {
+
+//  15. Time goal: 35 seconds. Code the first line of an if statement that tests whether a 
+//  variable has the value of a particular string.
+
+//  Answer:
+
+//  if (firstName === "Jane") {
+
+//  16. Time goal: 35 seconds. This is the first line of an if statement:
+
+//  if (x === y) {
+
+//  Complete the statement. If the condition is true, display a box that asks 
+//  the user a question. Assign the answer to a variable that hasn't been declared beforehand.
+
+//  Answer:
+
+//    var answer = prompt("What is z?");
+//  }
+
+//  17. Time goal: .. seconds. Code an if statement that tests whether pCode is "10010". If so, assign to the 
+//  variable city the string "New York". city has already been declared.
+
+//  Answer:
+
+//  if (pCode === "10010") {
+//       city = "New York";
+//  }
+
+//  18. Time goal: 45 seconds. Code an if statement. Test whether a variable has a particular numerical value. 
+//  If so, assign a new value to that variable, as in x = 1;
+
+//  Answer:
+
+//  if (num === 1) {
+//      num = 4;
+//  }
+
+//  19. Live coding exercise:
+//  - Code an if statement that tests whether a number equals itself.
+//  - If the condition is true (it will be), display a congratulations alert.
+//  - Click the Run It button to run your code live.
+
+//  Answer:
+
+//  if (4 === 4) {
+//      alert("Congratulations!");
+//  }
+
+//  20. Live coding exercise:
+//  - Code a prompt asking for your first name.
+//  - Code an if statement that tests whether the name you entered equals your name.
+//  - If the condition is true (it will be), display an alert that greets you by name.
+//  - Click the Run It button to run your code live.
+
+// Answer:
+
+//  var name = prompt("What is your first name?");
+//  if (name === "Sipo") {
+//      alert("Hi, " + name);
+//  }
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,7 @@
+x =  7;
+
+if (num === 1) {
+    num = 4;
+}
+
+console.log(x);
