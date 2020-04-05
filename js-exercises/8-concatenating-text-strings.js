83f13816d083c51e5f833869c5086dbcbd44fab5
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-05 06:50:10 +0100

    Concatenating text strings

---
+++ js-exercises/8-concatenating-text-strings.js
@@ -0,0 +1,141 @@
+//  Chapter 8 - Concatenating text strings
+//  ======================================
+
+//  1. var num = "2" + "2";
+//  What is the value of num? Include quotation marks.
+
+//  Answer:
+//  "22"
+
+//  2. What is it called when you combine two or more strings, using the plus sign?
+
+//  Answer:
+//  concatenation
+
+//  3. message = ("Hello," + "Dolly");
+//  What is the value of message? (Include the quotation marks.)
+
+//  Answer:
+//  "Holly,Dolly"
+
+//  4. alert("33" + 3);
+//  What message displays in the alert box?
+
+//  Answer:
+//  333
+
+//  5. Drag-and-drop. Holding down the left mouse button, drag the pieces to 
+//  create a statement that assigns to saying the concatenation of the two strings.
+
+//  Answer:
+//  var saying = "upsy-" + "daisy";
+
+//  6. Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+//  Write a statement that assigns to scribe the concatenation of the first and last names 
+//  of England's greatest playwright. The variable hasn't been declared beforehand.
+
+//  Answer:
+//  var scribe = "William" + "Shakespeare";
+
+//  7. Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+//  Write an alert that displays the concatenation of the two parts of "Woo hoo"
+
+//  Answer:
+//  alert("Woo " + "hoo");
+
+//  8. Write a statement that assigns to a variable the concatenation of the two parts of 
+//  "Oh yeah" (no period at the end). The second part is "yeah" The variable hasn't been 
+//  declared beforehand.
+
+//  Answer:
+//  var concatenation = "Oh " + "yeah";
+
+//  9. Write a statement that concatenates two variables and assigns the result to a 
+//  third variable. The third variable hasn't been declared beforehand.
+
+//  Answer:
+//  var fullName = firstName + lastName;
+
+//  10. Write a statement that displays an alert. The message is a string concatenated 
+//  with a variable.
+
+//  Answer:
+//  alert("Variables are " + unique);
+
+//  11. Write a statement that assigns to a variable the concatenation of a string with a number. 
+//  The variable has been declared beforehand.
+
+//  Answer:
+//  concatenation = "Sipo is " + 34;
+
+//  12. Assign strings to two variables. Then concatenate them and assign the result to a 
+//  third variable. None of the variables have been declared beforehand.
+
+//  Answer:
+//  var firstName = "Sipo ";
+//  var lastName = "Charles;
+//  var fullName = firstName + lastName;
+
+//  13. Time goal: 20 seconds. (The timer starts when you click in the answer field.)
+//  message = ("Hello," + "Dolly");
+//  What is the value of message? (Include the quotation marks.)
+
+//  Answer:
+//  "Hello,Dolly"
+
+//  14. Time goal: 30 seconds. Write a statement that concatenates two variables and 
+//  assigns the result to a third variable. The third variable hasn't been declared beforehand.
+
+//  Answer:
+//  var fullName = firstName + lastName;
+
+//  15. Time goal: 35 seconds. Write a statement that displays an alert. 
+//  The message is a string concatenated with a variable.
+
+//  Answer:
+//  alert("Hello " + name);
+
+//  16. Time goal: 35 seconds. Write a statement that assigns to a variable the concatenation 
+//  of a string and a number. The variable has been declared beforehand.
+
+//  Answer:
+//  age = "Sipo is " + 34;
+
+//  17. Time goal: 40 seconds. Write a statement that assigns to scribe the concatenation 
+//  of the first and last names of England's greatest playwright 
+//  (including the space between them). The variable hasn't been declared beforehand.
+
+//  Answer:
+//  var scribe = "William " + "Shakespeare";
+
+//  18. Time goal: 60 seconds. Assign strings to two variables. 
+//  Then concatenate them and assign the result to a third variable. 
+//  None of the variables have been declared beforehand.
+
+//  Answer:
+//  var firstName = "Sipo";
+//  var lastName = "Charles";
+//  var fullName = firstName + lastName;
+
+//  19. Live coding exercise:
+//  - Assign a string to a variable.
+//  - Display an alert, specifying the concatenation of the variable plus a string as the message.
+//  - Click the Run It button to run your code live.
+//  - Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+//  Answer:
+//  var greeting = "Hello ";
+//  alert(greeting + "world!");
+
+//  20. Live coding exercise:
+//  - Assign two strings to two variables.
+//  - Assign to a third variable the concatenation of the first two variables.
+//  - Display an alert, specifying the third variable as the message.
+//  - Click the Run It button to run your code live.
+//  - Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+//  Answer:
+//  var firstName = "Sipo ";
+//  var lastName = "Charles";
+//  var fullName = firstName + lastName;
+//  alert(fullName);
