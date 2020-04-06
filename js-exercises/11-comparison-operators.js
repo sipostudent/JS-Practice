978a716f1ef77afd639dc0e2abc99d94fa66cb10
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-06 02:22:53 +0100

    Comparison operators

---
+++ js-exercises/11-comparison-operators.js
@@ -0,0 +1,118 @@
+//  Chapter 11 - Comparion operators
+
+//  1. ===, !==, >, and <= are ________ operators.
+
+//  Answer:
+//  comparison
+
+//  2. What is the comparison operator for is not equal to?
+
+//  Answer:
+//  !==
+
+//  3. What is the comparison operator for is greater than?
+
+//  Answer:
+//  >
+
+//  4. What is the comparison operator for is less than or equal to?
+
+//  Answer:
+//  <=
+
+//  5. Create this if statement: If a isn't equal to b, a is assigned the value of c.
+
+//  Answer:
+//  if (a !== b) {
+//      a = c;
+//  }
+
+//  6. Code the first line of an if statement that tests whether num1 is greater than num2.
+
+//  Answer:
+//  if (num1 > num2) {
+
+//  7. Code the first line of an if statement that tests whether num1 is greater than or equal to num2.
+
+//  Answer:
+//  if (num1 >= num2) {
+
+//  8. Code the first line of an if statement that tests whether one variable is unequal to another.
+
+//  Answer:
+//  if (num1 !== num2) {
+
+//  9. Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+
+//  Answer:
+//  if (num1 !== "Bread") {
+
+//  10. Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+//  Answer:
+//  if (num1 < 7) {
+
+//  11. Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+
+//  Answer:
+//  if (num1 >= num2) {
+
+//  12. Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+
+//  Answer:
+//  if ("Sarah" !== herName) {
+
+//  13. Time goal: 25 seconds. Code the first line of an if statement that tests whether one variable is unequal to another.
+
+//  Answer:
+// if (num1 !== num2) {
+
+//  14. Time goal: 30 seconds. Code the first line of an if statement that tests whether a variable has a different value from a particular string.
+
+//  Answer:
+//  if (gender !== "female") {
+
+//  15. Time goal: 25 seconds. Code the first line of an if statement that tests whether a variable is less than a particular number.
+
+//  Answer:
+//  if (num1 < 7) {
+
+//  16. Time goal: 25 seconds. Code the first line of an if statement that tests whether the value represented by a variable is greater than or equal to the value represented by another variable.
+
+//  Answer:
+//  if (num1 >= num2) {
+
+//  17. Time goal: 30 seconds. Code the first line of an if statement that tests whether a string is unequal to the value represented by a particular variable.
+
+//  Answer:
+//  if ("Hello" !== NoGreeting) {
+
+//  18. Time goal: 45 seconds. Code an if statement. Test whether a variable is unequal to a particular number. If so, assign a number to that variable.
+
+//  Answer:
+
+//  if (num1 !== 7) {
+//      num1 = 5;
+//  }
+
+//  19. Live coding exercise:
+//  - Code an if statement that tests whether a number is unequal to a different number.
+//  - If the condition is true (it will be), display a congratulations alert.
+//  - Click the Run It button to run your code live.
+
+//  Answer:
+//  if (5 !== 7) {
+//      alert("Congratulations!");
+//  }
+
+//  20. Live coding exercise:
+//  - Code a prompt asking for your first name.
+//  - Code an if statement that tests whether the name you entered is unequal to another name.
+//  - If the condition is true (it will be), display an alert that says "No match"
+//  - Click the Run It button to run your code live.
+
+//  Answer:
+//  var name = prompt("What is your first name?");
+//  if (name !== "Sipo") {
+//      alert("No match");
+//  }
--- test.js
+++
@@ -1,7 +0,0 @@
-x =  7;
-
-if (num === 1) {
-    num = 4;
-}
-
-console.log(x);
