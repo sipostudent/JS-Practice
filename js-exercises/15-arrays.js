542fa68f2166adfe1f54a29e1e299a69d5167dd3
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-07 12:31:56 +0100

    Arrays

---
+++ js-exercises/15-arrays.js
@@ -0,0 +1,134 @@
+//  Chapter - 15 Arrays
+//  ===================
+
+//  1. What is a variable that represents a list of values? Answer with one word.
+
+//  Answer:
+
+//  array
+
+//  2. An array comprises any number of ______. ("Values" would be correct, but I'm looking for another word.)
+
+//  Answer:
+
+//  elements
+
+//  3. The number inside the square brackets is the element's ____.
+
+//  Answer:
+
+//  index
+
+//  4. In an array, the numbering starts with __.
+
+//  Answer:
+
+//  0
+
+//  5. Create a new array of strings.
+
+//  Answer:
+
+//  var numsAsStrings = ["1", "2", "3"];
+
+//  6. Create a new array, nums, comprising three numbers from 3 to 5.
+
+//  Answer:
+
+//  var nums = [3, 4, 5];
+
+//  7. This array has been defined.
+
+//  var names = ["Alf", "Bo", "Gail"];
+
+//  Specifying an array element, code an alert that displays "Gail".
+
+//  Answer:
+
+//  alert(names[2]);
+
+//  8. Create an array with a single element, a string. The array hasn't been declared beforehand.
+
+//  Answer:
+
+//  var listOne = ["Pop"];
+
+//  9. Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+//  Answer:
+
+//  var list = [1, 2, 3];
+
+//  10. Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+
+//  Answer:
+
+//  var varOne = mixedArray[0];
+
+//  11. Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+//  Answer:
+
+//  arrayOne = [1, "Pop", chicken];
+
+//  12. Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+//  Answer:
+
+// var arrayOne = ["Stop", "Pop"];
+// var lastElement = arrayOne[1];
+
+//  13. Time goal: 30 seconds.
+//  - This array has been defined.
+//  - var names = ["Alf", "Bo", "Gail"];
+//  - Specifying an array element, code an alert that displays "Gail".
+
+//  Answer:
+
+//  alert(names[2]);
+
+//  14. Time goal: 30 seconds. Create an array with one string element. The array hasn't been declared beforehand.
+
+//  Answer:
+
+//  var arrayOne = ["Pop"];
+
+//  15. Time goal: 30 seconds. Create an array with three elements that are numbers. It hasn't been declared beforehand.
+
+//  var nums = [1, 2, 3];
+
+//  16. Time goal: 30 seconds. Assign to a variable the first element of an array. The variable hasn't been declared beforehand.
+
+//  Answer:
+
+//  var num = list[0];
+
+//  17. Time goal: 50 seconds. Create an array with three elements: a number, a string, and a variable. The array has been declared beforehand.
+
+//  Answer:
+
+//  mixedArray = [1, "Pop", cheese];
+
+//  18. Time goal: 60 seconds. Create an array with two string elements. Then assign the last element in the array to a variable. Neither the array nor the variable have been declared beforehand.
+
+//  Answer:
+
+// var arrayOne = ["Stop", "Pop"];
+// var lastElement = arrayOne[1];
+
+//  19. Live coding exercise:
+//  - Declare an array with three elements.
+//  - Display the last element in an alert.
+//  - Click the Run It button to run your code live.
+
+// var mixedArray = [1, 2, 3];
+// alert(mixedArray[2]);
+
+//   20. Live coding exercise:
+//  - Declare an array with four elements.
+//  - Display the first element in an alert.
+//  - Click the Run It button to run your code live.
+
+// var fourArray = [1, 2, 3, 4];
+// alert(fourArray[0]);
+  
