8c7f306f8ba560a79f3b7ec5e1e71418f193a596
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-07 13:53:16 +0100

    Arrays: adding and removing elements

---
+++ js-exercises/16-arrays-adding-and-removing-elements.js
@@ -0,0 +1,137 @@
+//  Chapter 16 - Arrays: adding and removing elements
+//  =================================================
+
+//  1. This code declares an empty array. Complete it.
+//  var finalScores = ____
+
+//  Answer:
+
+//  [];
+
+//  2. counties is an empty array. What is the value of counties[0]?
+
+//  Answer:
+
+//  undefined
+
+//  3. What keyword removes the last element from an array?
+
+//  Answer:
+
+//  pop
+
+//  4. What keyword adds one or more elements to the end of an array?
+
+//  Answer:
+
+//  push
+
+//  5. Create a new empty array.
+
+//  Answer:
+
+//  var fastestTimes = [];
+
+//  6. Assign the number 1 to the 10th element of the array nums.
+
+//  Answer:
+
+//  nums[9] = 1;
+
+//  7. Remove the last element from the array pets.
+
+//  Answer:
+
+//  pets.pop();
+
+//  8. Create an empty array.
+
+//  Answer:
+
+//  var pets = [];
+
+//  9. Change the value of the first element of an array to a new string.
+
+//  Answer:
+
+//  pets[0] = "Lizard";
+
+//  10. Add a new element, a number, to the end of an array.
+
+//  Answer:
+
+//  pets.push(1);
+
+//  11. Assign string values to the second and fourth elements of an array.
+
+//  Answer:
+
+//  names[1] = "Crackle";
+//  names[3] = "Pop";
+
+//  12. Add three number elements to end of an array.
+
+//  Answer:
+
+//  num.push(5, 7, 3);
+
+//  13. Time goal: 25 seconds. Assign the number 1 to the 10th element of the array nums.
+
+//  Answer:
+
+//  nums[9] = 1;
+
+//  14. Time goal: 25 seconds. Create an empty array.
+
+//  Answer:
+
+//  var nums = [];
+
+//  15. Time goal: 25 seconds. Change the value of the first element of an array to a new string.
+
+//  Answer:
+
+//  arr[0] = "Crackle";
+
+//  16. Time goal: 25 seconds. Add a new element, a number, to the end of an array.
+
+//  Answer:
+
+//  arr.push(7);
+
+//  17. Time goal: 50 seconds. Assign string values to the second and fourth elements of an array.
+
+//  Answer:
+
+//  arr[1] = "Snap";
+//  arr[3] = "Crackle";
+
+//  18. Time goal: 35 seconds. Add three number elements to the end of an array.
+
+//  Answer:
+
+//  arr.push(3, 4, 5);
+
+//  19. Live coding exercise:
+//  - Declare an empty array.
+//  - Assign a value to one of its elements.
+//  - Create an alert whose message is the element.
+//  - Click the Run It button to run your code live.
+
+//  Answer:
+
+// var arr = [];
+// arr[0] = "Snap";
+// alert(arr[0]);
+
+//  20. Live coding exercise:
+//  - Code an array with 1 string element.
+//  - Add an additional element to the array using push.
+//  - Create an alert whose message is the last element.
+//  - Click the Run It button to run your code live.
+
+//  Answer
+
+// var arr = ["Crackle"];
+// arr.push("Pop");
+// alert(arr[1]);
