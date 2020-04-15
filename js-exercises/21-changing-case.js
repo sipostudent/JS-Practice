d75b871bbe1892108454b6bb8314344909746d7c
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-15 23:58:55 +0100

    Changing Case

---
+++ js-exercises/21-changing-case.js
@@ -0,0 +1,30 @@
+//  Chapter 21 - Changing case
+//  ==========================
+
+//  1. Type the characters that are missing from this code.
+//  var allLower = userInput.toLowerCase;
+
+//  Answer:
+
+//  ()
+
+//  2. Type the character that is missing from this code.
+//  var allLower = userInput toLowerCase();
+
+//  Answer:
+
+//  .
+
+//  3. Two characters in this code are incorrect. Type the two correct characters.
+//  var allLower = userInput.tolowercase();
+
+//  Answer:
+
+//  LC
+
+//  4. What is the value of lowerVersion? Include the quotation marks in your answer.
+//  var lowerVersion = "NO!".toLowerCase();
+
+//  5. Answer:
+
+//  "no!"
